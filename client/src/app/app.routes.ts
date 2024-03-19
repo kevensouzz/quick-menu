5235ef49011340e268a819d31b5a4ceca05b8968
kevensouzz <souzadossantoskeven@gmail.com>
2024-03-19 15:36:41 -0300

    dockerize client interface

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -6,9 +6,9 @@ import { AuthComponent } from './pages/auth/auth.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "scan", component: ScanComponent},
-  {path: "generate", component: GenerateComponent},
-  {path: "auth", component: AuthComponent},
-  {path: "**", component: NotFoundComponent},
+  { path: "", component: HomeComponent },
+  { path: "scan", component: ScanComponent },
+  { path: "generate", component: GenerateComponent },
+  { path: "auth", component: AuthComponent },
+  { path: "**", component: NotFoundComponent }
 ];
