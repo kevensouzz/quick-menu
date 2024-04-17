4f40a3dac8e297220c5bde93f05ecee6bc05de00
kevensouzz <souzadossantoskeven@gmail.com>
2024-04-17 17:42:47 -0300

    create menu controller

--- server/src/main/java/br/com/kevensouza/server/controllers/MenuController.java
+++ server/src/main/java/br/com/kevensouza/server/controllers/MenuController.java
@@ -33,7 +33,7 @@ public class MenuController {
         if (body.getName().isEmpty() || body.getCode().isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
         } else if (!body.getCode().matches("[A-Z0-9]{6}")) {
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("code must have 6 characters including numbers and letters.");
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("code must have 6 characters (numbers | uppercase letters).");
         } else if (menuRepository.findByCode(body.getCode()).isPresent()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("this code is already in use.");
         }
@@ -42,7 +42,7 @@ public class MenuController {
         return ResponseEntity.status(HttpStatus.CREATED).body("successfully created!");
     }
 
-    @GetMapping("/all")
+    @GetMapping
     public ResponseEntity<List<MenuModel>> List() {
         List<MenuModel> menus = menuRepository.findAll();
         if (!menus.isEmpty()) {
@@ -54,7 +54,7 @@ public class MenuController {
         return ResponseEntity.status(HttpStatus.OK).body(menus);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/id/{id}")
     public ResponseEntity<MenuModel> ListById(@PathVariable(value = "id") UUID id) {
         Optional<MenuModel> menu = menuRepository.findById(id);
         if (menu.isEmpty()) {
@@ -62,5 +62,47 @@ public class MenuController {
         }
         menu.get().add(linkTo(methodOn(MenuController.class).List()).withSelfRel());
         return ResponseEntity.status(HttpStatus.OK).body(menu.get());
+    }
+
+    @GetMapping("/code/{code}")
+    public ResponseEntity<MenuModel> ListByCode(@PathVariable(value = "code") String code) {
+        Optional<MenuModel> menu = menuRepository.findByCode(code);
+        if (menu.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        menu.get().add(linkTo(methodOn(MenuController.class).List()).withSelfRel());
+        return ResponseEntity.status(HttpStatus.OK).body(menu.get());
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> Update(@PathVariable(value = "id") UUID id, @RequestBody @Valid MenuModel body) {
+        Optional<MenuModel> menu = menuRepository.findById(id);
+        if (menu.isEmpty()) {
+            return ResponseEntity.status((HttpStatus.NOT_FOUND)).build();
+        } else if (menuRepository.findByCode(body.getCode()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+        if (body.getName() != null) {
+            menu.get().setName(body.getName());
+        }
+
+        if (body.getCode() != null) {
+            menu.get().setCode(body.getCode());
+        }
+
+        menuRepository.save(menu.get());
+        return ResponseEntity.status(HttpStatus.OK).body("successfully updated!");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> Delete(@PathVariable(value = "id") UUID id) {
+        Optional<MenuModel> menu = menuRepository.findById(id);
+        if (menu.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        menuRepository.delete(menu.get());
+        return ResponseEntity.status(HttpStatus.OK).body("successfully deleted!");
     }
 }
